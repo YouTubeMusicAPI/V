0dc17b85a99ec7d3da12031086cae308e1cb44c3
YouTubeMusicAPI <FastMusicYTAPI@gmail.com>
2026-02-03 00:10:56 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+from Test import user, bot
+
+app = PyTgCalls(user)
+
+@bot.on_message(filters.command("play"))
+async def play(_, m):
+
+    ok = await ensure_assistant(bot, user, m.chat.id, m)
+    if not ok:
+        return
+
+    reply = m.reply_to_message
+
+    # ─── CASE 1: Reply to voice / audio ───
+    if reply and (reply.voice or reply.audio):
+
+        await m.reply("🎧 Voice / Audio detected, processing...")
+
+        # Download telegram file
+        file_path = await reply.download()
+
+        await app.play(
+            m.chat.id,
+            file_path,
+        )
+
+      
